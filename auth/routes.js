bfce90aac42a513dae184c2fe4347a856ae34a3f
Bram Koot <git@bramkoot.nl>
2018-09-05 16:25:31 +0200

    Add email/password check in login route

--- auth/routes.js
+++ auth/routes.js
@@ -3,6 +3,7 @@ const {toJWT, toData} = require('./jwt')
 const User = require('../users/model')
 const router = new Router()
 const auth = require('./middleware')
+const bcrypt = require('bcrypt')
 
 router.post('/logins', (req, res) => {
   const email = req.body.email
@@ -14,10 +15,39 @@ router.post('/logins', (req, res) => {
     })
   }
   else {
-    // normally we would check the password here, now we just send a JWT
-    res.send({
-      jwt: toJWT({ userId: 1 })
-    })
+    User
+      .findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+      .then(entity => {
+        if (!entity) {
+          res.status(400).send({
+            message: 'User with that email does not exist'
+          })
+        }
+
+        // 2. use bcrypt.compareSync to check the password against the stored hash
+        else if (bcrypt.compareSync(req.body.password, entity.password)) {
+
+          // 3. if the password is correct, return a JWT with the userId of the user (user.id)
+          res.send({
+            jwt: toJWT({ userId: entity.id })
+          })
+        }
+        else {
+          res.status(400).send({
+            message: 'Password was incorrect'
+          })
+        }
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(500).send({
+          message: 'Something went wrong'
+        })
+      })
   }
 })
 
