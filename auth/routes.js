c98d03605e27fec64fb1e95640fab6c32aacc845
Sil Westerveld <git@silwesterveld.com>
2018-11-14 14:37:01 +0100

    Add route for secret-endpoint

--- auth/routes.js
+++ auth/routes.js
@@ -3,6 +3,29 @@ const { toJWT, toData } = require('./jwt')
 
 const router = new Router()
 
+router.get('/secret-endpoint', (req, res) => {
+  const auth = req.headers.authorization && req.headers.authorization.split(' ')
+  if (auth && auth[0] === 'Bearer' && auth[1]) {
+    try {
+      const data = toData(auth[1])
+      res.send({
+        message: 'Thanks for visiting the secret endpoint.',
+        data
+      })
+    }
+    catch(error) {
+      res.status(400).send({
+        message: `Error ${error.name}: ${error.message}`,
+      })
+    }
+  }
+  else {
+    res.status(401).send({
+      message: 'Please supply some valid credentials'
+    })
+  }
+})
+
 router.post('/logins', (req, res, next) => {
   const {email,password} = req.body
 
