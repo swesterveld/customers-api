30b93ee2b0a019199a7ebb2f338cbca6e614c352
Sil Westerveld <git@silwesterveld.com>
2018-11-14 16:59:48 +0100

    Add middleware for authentication

--- auth/routes.js
+++ auth/routes.js
@@ -1,31 +1,15 @@
 const { Router } = require('express')
-const { toJWT, toData } = require('./jwt')
+const { toJWT } = require('./jwt')
 const User = require('../users/model')
 const bcrypt = require('bcrypt')
+const auth = require('../auth/middleware')
 
 const router = new Router()
 
-router.get('/secret-endpoint', (req, res) => {
-  const auth = req.headers.authorization && req.headers.authorization.split(' ')
-  if (auth && auth[0] === 'Bearer' && auth[1]) {
-    try {
-      const data = toData(auth[1])
-      res.send({
-        message: 'Thanks for visiting the secret endpoint.',
-        data
-      })
-    }
-    catch(error) {
-      res.status(400).send({
-        message: `Error ${error.name}: ${error.message}`,
-      })
-    }
-  }
-  else {
-    res.status(401).send({
-      message: 'Please supply some valid credentials'
-    })
-  }
+router.get('/secret-endpoint', auth, (req, res) => {
+  res.send({
+    message: `Thanks for visiting the secret endpoint ${req.user.email}.`,
+  })
 })
 
 router.post('/logins', (req, res, next) => {
