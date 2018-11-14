7c3619529460569a7cc34b79d8bb2b0a3123ef7b
Sil Westerveld <git@silwesterveld.com>
2018-11-14 16:07:05 +0100

    Implement login with token; when user exists and the (hashed) password matches with the one that has been stored, return a JWT token

--- auth/routes.js
+++ auth/routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express')
 const { toJWT, toData } = require('./jwt')
+const User = require('../users/model')
+const bcrypt = require('bcrypt')
 
 const router = new Router()
 
@@ -30,10 +32,40 @@ router.post('/logins', (req, res, next) => {
   const {email,password} = req.body
 
   if (email && password) {
-    // normally we would check the password here, now we just send a JWT
-    return res.send({
-      jwt: toJWT({userId: 1})
-    })
+    // 1. find user based on email address
+    User
+      .findOne({
+        where: {
+          email: email
+        }
+      })
+      .then(entity => {
+        if (!entity) {
+          res.status(400).send({
+            message: 'User with that email does not exist'
+          })
+        }
+
+        // 2. use bcrypt.compareSync to check the password against the stored hash
+        if (bcrypt.compareSync(password, entity.password)) {
+
+          // 3. if the password is correct, return a JWT with the userId of the user (user.id)
+          res.send({
+            jwt: toJWT({userId: entity.id})
+          })
+        }
+        else {
+          res.status(400).send({
+            message: 'Password was incorrect'
+          })
+        }
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(500).send({
+          message: 'Something went wrong'
+        })
+      })
   }
   else {
     return res.status(400).send({
