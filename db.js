766529341a291e1ac4683429fed087e8c523d01e
Sil Westerveld <git@silwesterveld.com>
2018-11-14 12:40:38 +0100

    Add relationship between customer and company

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
-const sequelize = new Sequelize(connectionString, {define: { timestamps: false }})
+const sequelize = new Sequelize(connectionString, {define: { timestamps: false }, force: true})
 
 sequelize.sync()
   .then(() => {
